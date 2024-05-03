3b76ac9d75149fdd6b55020cba9c3a50a6e45622
Valentin Berlin <valentin.berlin@cabify.com>
2024-05-03 17:54:06 +0200

    add language feature

--- go-fundamentals/hello/hello.go
+++ go-fundamentals/hello/hello.go
@@ -2,16 +2,44 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix = "Hello, "
+const (
+	spanish = "ES"
+	french  = "FR"
+	german  = "DE"
 
-func Hello(name string) string {
+	englishHelloPrefix = "Hello"
+	spanishHelloPrefix = "Hola"
+	frenchHelloPrefix  = "Bonjour"
+	germanHelloPrefix  = "Guten tag"
+)
+
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "world"
 	}
 
-	return englishHelloPrefix + name
+	return greetingPrefix(language) + ", " + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+
+	case french:
+		prefix = frenchHelloPrefix
+
+	case german:
+		prefix = germanHelloPrefix
+
+	default:
+		prefix = englishHelloPrefix
+
+	}
+
+	return
 }
 
 func main() {
-	fmt.Println(Hello("Bob"))
+	fmt.Println(Hello("Bob", ""))
 }
--- go-fundamentals/hello/hello_test.go
+++ go-fundamentals/hello/hello_test.go
@@ -4,17 +4,41 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Valya")
+		got := Hello("Valya", "")
 		want := "Hello, Valya"
 
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say hello world when no name is given", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, world"
 
 		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Pablo", "ES")
+		want := "Hola, Pablo"
+
+		assertCorrectMessage(t, got, want)
+
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Camile", "FR")
+		want := "Bonjour, Camile"
+
+		assertCorrectMessage(t, got, want)
+
+	})
+
+	t.Run("in German", func(t *testing.T) {
+		got := Hello("Gustav", "DE")
+		want := "Guten tag, Gustav"
+
+		assertCorrectMessage(t, got, want)
+
 	})
 }
 
