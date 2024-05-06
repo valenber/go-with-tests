dbbb5f0c1335528674d1de4b25d4dd0e27d91b11
Valentin Berlin <valentin.berlin@cabify.com>
2024-05-06 13:01:37 +0200

    structs, methods and interfaces

---
+++ go-fundamentals/structs/shapes_test.go
@@ -0,0 +1,38 @@
+package structs
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+type Shape interface {
+	Area() float64
+}
+
+func TestArea(t *testing.T) {
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.00},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
+	}
+
+	for _, tc := range areaTests {
+		t.Run(tc.name, func(t *testing.T) {
+			got := tc.shape.Area()
+			if got != tc.hasArea {
+				t.Errorf("%#v got %g wanted %g", tc.shape, got, tc.hasArea)
+			}
+		})
+	}
+}
